aa4c66a75bf3e59a1406db88fe55305ca2cecfbd
jagodalewandowska <45319683+jagodalewandowska@users.noreply.github.com>
2024-08-27 15:40:46 +0200

    Delete App/config.py

--- App/config.py
+++
@@ -1,8 +0,0 @@
-SECRET_KEY = '5524d09c5c9cb4d189d2a8fc'
-DATABASE_CONFIG = {
-    'dbname': 'bezpieczenstwo-piatek',
-    'user': 'postgres',
-    'password': 'politechnika',
-    'host': 'localhost',
-    'port': '5433'
-}
